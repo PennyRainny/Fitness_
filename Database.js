93829f5a93c73c9e8cca0a9c245171b44c41fd6e
Thanawat Satdee <165295189+PennyRainny@users.noreply.github.com>
2025-04-03 21:57:31 +0700

    Update Database.js

--- Database.js
+++ Database.js
@@ -4,10 +4,10 @@ const mssql = require("mssql");
 const dbConnect = async () => {
   try {
     const pool = await mssql.connect({
-      user: "konglovecpe",     // เปลี่ยนเป็น username ของคุณ
-      password: "0881795204konG", // เปลี่ยนเป็น password ของคุณ
-      server: "konglovecpe.database.windows.net",      // หรือ server ที่คุณใช้งาน
-      database: "FITNESS451", // ชื่อ database ที่จะเชื่อมต่อ
+      user: ".....",     // เปลี่ยนเป็น username ของคุณ
+      password: "....", // เปลี่ยนเป็น password ของคุณ
+      server: "....",      // หรือ server ที่คุณใช้งาน
+      database: "....", // ชื่อ database ที่จะเชื่อมต่อ
       options: {
         encrypt: true, 
         trustServerCertificate: false, 

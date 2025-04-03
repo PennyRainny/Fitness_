ae58486186bef71304b3e08305ce21241077a7f0
PennyRainny <bigonlinehard@gmail.com>
2025-04-03 21:48:19 +0700

    อัปเดตโค้ด

--- Database.js
+++ Database.js
@@ -1,4 +1,29 @@
+
 const mssql = require("mssql");
+
+const dbConnect = async () => {
+  try {
+    const pool = await mssql.connect({
+      user: "konglovecpe",     // เปลี่ยนเป็น username ของคุณ
+      password: "0881795204konG", // เปลี่ยนเป็น password ของคุณ
+      server: "konglovecpe.database.windows.net",      // หรือ server ที่คุณใช้งาน
+      database: "FITNESS451", // ชื่อ database ที่จะเชื่อมต่อ
+      options: {
+        encrypt: true, 
+        trustServerCertificate: false, 
+      },
+    });
+    console.log("Connected to MSSQL database!");
+    return pool;
+  } catch (err) {
+    console.error("Database connection failed: ", err);
+    throw err;
+  }
+};
+
+module.exports = dbConnect;
+
+/*const mssql = require("mssql");
 const mysql = require("mysql2");
 //const dbConnection = mssql.connect({
 const dbConnect = mysql.createPool({
@@ -8,4 +33,5 @@ const dbConnect = mysql.createPool({
     database : "testdatabase",
 }).promise()
 
-module.exports = dbConnect;
+module.exports = dbConnect;*/
+
